d93bc96e1ef25ec4a00114bb2e501070bca552c9
Collin Irwin <irwincollin+github@gmail.com>
2016-01-03 18:57:01 -0500

    Changed body part detection back to the normal image. Trying out thresholding and blob detection with diff. (BlobDetect not working)

--- main.py
+++ main.py
@@ -34,12 +34,11 @@ upperBodyCascade = cv2.CascadeClassifier(path + "haarcascade_upperbody.xml")
 lowerBodyCascade = cv2.CascadeClassifier(path + "haarcascade_lowerbody.xml")
 
 # instantiate useful classes
-bgs = cv2.BackgroundSubtractorMOG()
+detector = cv2.SimpleBlobDetector()
 
-#Load and prep bgs with 0-person control image
+# Cam number and image path
 cam = "cam2"
 emptyCam = cv2.imread("control - uf rec/" + cam + "[3].jpg")
-bgs.apply(emptyCam) # prep the bgs with empty image
 
 failures = 0
 i = 0
@@ -65,24 +64,26 @@ while i < 60:
         diff = None
         if t_minus is not None and t is not None and t_plus is not None:
             diff = diffImg(t_minus, t, t_plus)
+            ret,diff = cv2.threshold(diff,30,255,cv2.THRESH_BINARY)
+            keypoints = detector.detect(diff)
+            diff = cv2.drawKeypoints(diff, keypoints, np.array([]), (255,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # TODO: find body part moving average over past 20 checks
-        if diff is not None:
-            #Find body parts
-            faces = getParts(diff, faceCascade)
-            profiles = getParts(diff, profileCascade)
-            fullBodies = getParts(diff, fullBodyCascade)
-            lowerBodies = getParts(diff, lowerBodyCascade)
-            upperBodies = getParts(diff, upperBodyCascade)
-            
-            # Draw rectangles around the body parts
-            drawParts(fullBodies, (255, 128, 128), diff)
-            drawParts(upperBodies, (255, 255, 0), diff)
-            drawParts(lowerBodies, (255, 0, 255), diff)
-            drawParts(faces, (0, 255, 0), diff)
-            drawParts(profiles, (0, 0, 255), diff)
+        #Find body parts
+        faces = getParts(img, faceCascade)
+        profiles = getParts(img, profileCascade)
+        fullBodies = getParts(img, fullBodyCascade)
+        lowerBodies = getParts(img, lowerBodyCascade)
+        upperBodies = getParts(img, upperBodyCascade)
+        
+        # Draw rectangles around the body parts
+        drawParts(fullBodies, (255, 128, 128), img)
+        drawParts(upperBodies, (255, 255, 0), img)
+        drawParts(lowerBodies, (255, 0, 255), img)
+        drawParts(faces, (0, 255, 0), img)
+        drawParts(profiles, (0, 0, 255), img)
 
-            print "Parts Found: " + str(len(upperBodies)) + ", " + str(len(lowerBodies))  + ", " + str(len(faces))  + ", " + str(len(profiles))
+        print "Parts Found: " + str(len(upperBodies)) + ", " + str(len(lowerBodies))  + ", " + str(len(faces))  + ", " + str(len(profiles))
 
         # Display the resulting images
         if diff is not None:
